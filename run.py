f391bd2ba16e46808eb2d8c0c49e74bb43bdab45
johnvenkiah <contact@johnvenkiah.com>
2021-07-14 16:00:57 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,1 +1,34 @@
-# python code goes here
+
+import datetime
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/calendar.events'
+    ]
+
+CREDS = Credentials.from_service_account_file('credentials.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+
+service = build('calendar', 'v3', credentials=CREDS)
+
+
+def main():
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='uueq3s2tbgdl57dvmmvcp5osd8@group.calendar.google.com', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+main()
