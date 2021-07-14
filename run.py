79a04246547ecc23b8e6f2abb1874ddd9af4f62c
johnvenkiah <contact@johnvenkiah.com>
2021-07-14 22:20:31 +0000

    setup: consolidate py files to one with all functionality

--- run.py
+++ run.py
@@ -1,24 +1,41 @@
-
+from __future__ import print_function
 import datetime
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from apiclient.discovery import build
+from httplib2 import Http
 from google.oauth2.service_account import Credentials
 
-SCOPE = [
-    'https://www.googleapis.com/auth/calendar',
-    ]
-
+SCOPES = 'http://www.googleapis.com/auth/calendar'
 CREDS = Credentials.from_service_account_file('credentials.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-
+SCOPED_CREDS = CREDS.with_scopes(SCOPES)
 CAL = build('calendar', 'v3', credentials=CREDS)
 
-GMT_OFF = '-00.00'
+GMT_OFF = '-00:00'
+EVENT = {
+    "start": {
+        "dateTime": '2021-07-22T15:00:00Z',
+        },
+    "end": {
+        "dateTime": '2021-07-22T16:00:00Z',
+        },
+    'summary': 'John Locke, Neckpain',
+}
+
+e = CAL.events().insert(
+        calendarId='uueq3s2tbgdl57dvmmvcp5osd8@group.calendar.google.com',
+        sendNotifications=True, body=EVENT).execute()
+
+print('''*** %r event added:
+    Start: %s
+    End: %s''' % (e['summary'].encode('utf-8'),
+        e['start']['dateTime'], e['end']['dateTime']))
 
 # Call the Calendar API
-now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
 print('Getting the upcoming 10 events')
+events_result = CAL.events().list(
+    calendarId='uueq3s2tbgdl57dvmmvcp5osd8@group.calendar.google.com', timeMin=now,
+    maxResults=10, singleEvents=True,
+    orderBy='startTime').execute()
 
 events_result = CAL.events().list(
     calendarId='uueq3s2tbgdl57dvmmvcp5osd8@group.calendar.google.com',
@@ -33,23 +50,3 @@ if not events:
 for event in events:
     start = event['start'].get('dateTime', event['start'].get('date'))
     print(start, event['summary'])
-
-
-EVENT = {
-    "start": {
-        "dateTime": '2021-07-22T15:00:00Z',
-        },
-    "end": {
-        "dateTime": '2021-07-22T16:00:00Z',
-        },
-    'summary': 'John Locke, Neckpain',
-}
-
-e = CAL.events().insert(
-    calendarId='uueq3s2tbgdl57dvmmvcp5osd8@group.calendar.google.com',
-    sendNotifications='True', body='EVENT').execute()
-
-print('''%r event added:
-    Start: %s
-    End: %s''' % (e['summary'].encode('utf-8'),
-        e['start']['dateTime'], e['end']['dateTime']))
---
+++ todo.md
@@ -0,0 +1,2 @@
+- Create function that parses users input into dictionary
+- Find way to suggest appointment that doesn't clash with other one
