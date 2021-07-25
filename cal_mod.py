abe2b1c83106aa83a0a226d21b8bc4b2b356c9a1
johnvenkiah <contact@johnvenkiah.com>
2021-07-25 18:06:15 +0000

    python: add docstrings to cal_mod, add screenshots validation

--- cal_mod.py
+++ cal_mod.py
@@ -1,4 +1,19 @@
+"""
+cal_mod, Calendar operations module for Feegood Physio Application
+Contains functions to call Google API Calendar and get or recieve data to be
+passed to the functions in run.py.
+"""
+
+
 def apt_list(cal, c_id, start, end):
+    """
+    Gets a list of event dictionaries from the Calendar.
+
+    @param cal(resource): The calendar being called
+    @param c_id(str): The calendar ID
+    @param start(str): The start time of the period or events
+    @param end(str): The end time of the period or events
+    """
 
     appointments_result = cal.events().list(
         calendarId=c_id,
@@ -13,6 +28,13 @@ def apt_list(cal, c_id, start, end):
 
 
 def del_apt(cal, c_id, apt_id):
+    """
+    Deletes a specific event from the Google calendar chosen by user.
+
+    @param cal(resource): The calendar being called
+    @param c_id(str): The calendar ID
+    @param apt_id(str): The Google event ID of the specific event to be deleted
+    """
 
     cal.events().delete(  # pylint: disable=maybe-no-member
         calendarId=c_id,
@@ -21,6 +43,14 @@ def del_apt(cal, c_id, apt_id):
 
 
 def get_apt(cal, c_id, apt_id):
+    """
+    Gets a specific event from the Google Calendar.
+
+    @param cal(resource): The calendar being called
+    @param c_id(str): The calendar ID
+    @param apt_id(str): The Google event ID of the specific event to be
+        retreived
+    """
 
     apntmnt_to_edit = cal.events().get(  # pylint: disable=maybe-no-member
         calendarId=c_id,
@@ -34,8 +64,8 @@ def updt_apt(cal, c_id, apt_id, apmnt):
     Update the Google Calendar event with the input from the previous
     functions from user.
 
-    @param apntmnt_id(str): Google Calendar event identifier
-    @param apntmnt_to_edit: Instance of CAL-resource updating calendar
+    @param apt_id(str): Google Calendar event identifier
+    @param apmnt(dict): The appointment passed to the calendar
     """
     cal.events().update(  # pylint: disable=maybe-no-member
         calendarId=c_id,
@@ -45,7 +75,13 @@ def updt_apt(cal, c_id, apt_id, apmnt):
 
 
 def insrt_apt(cal, c_id, evnt):
+    """
+    Inserts a new event to the Google Calendar, from the user input.
 
+    @param cal(resource): The calendar being called
+    @param c_id(str): Google Calendar ID
+    @param evnt(dict): Event inserted into calendar
+    """
     cal.events().insert(  # pylint: disable=maybe-no-member
         calendarId=c_id,
         sendNotifications=True, body=evnt
